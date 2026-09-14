894bfa2c29db5198e8ef660e7b33498216d1899b
Juanjo Trabajo <josevargas070287@gmail.com>
2026-09-14 16:28:37 -0400

    configuracion del main

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,34 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { createObserveModule } from '@nestjs/observe';
-import { AppController } from './app.controller.js';
-import { AppService } from './app.service.js';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 export const { ObserveModule, ObserveInstrument } = createObserveModule();
 
 @Module({
   imports: [
-    // Distributed tracing, auto-correlated logs, request/job metrics, error
-    // telemetry, alarms, and more — out of the box. Sign up at https://observe.nestjs.com
+
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASWORD,
+      entities: [import.meta.dirname + '/**/*.entity{.ts,.js}'],
+      logging: true
+    }),
+    
     ObserveModule.forRoot({
       appKey: 'YOUR_APP_KEY',
       appSecret: 'YOUR_APP_SECRET',
       serviceId: 'api-cine',
     }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule {
+
+}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule, ObserveInstrument } from './app.module.js';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { METHODS } from 'http';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    instrument: ObserveInstrument,
+  const app = await NestFactory.create(AppModule);
+  const logger = new Logger('Bootstrap');
+
+  //configuración para el front end
+  app.enableCors({
+    origin: true,
+    METHODS: ['GET','POST','PUT','PATCH','DELETE'],
+    Credential: true,
+    allwedHeaders: 'Content-Type, Accept, Authorization'
   });
+  
+  app.setGlobalPrefix('api/v1');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted:true
+    })
+  );
   await app.listen(process.env.PORT ?? 3000);
+  logger.log(`La aplicación esta correindo en: ${await app.getUrl()}`)
 }
 await bootstrap();
